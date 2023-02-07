2a486d6757ab98f612504ceecd1bbc02e1f32164
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2023-02-07 17:38:59 +0000

    Simplified TCR standardisation implemented for Homo sapiens

--- src/tidytcells/tcr.py
+++ src/tidytcells/tcr.py
@@ -16,244 +16,68 @@ from warnings import warn
 
 with resource_stream(
     __name__,
-    'resources/tcr_alleles_homosapiens.json') as s:
-    _TCR_ALLELES_HOMOSAPIENS = json.load(s)
+    'resources/homosapiens_tcr.json') as s:
+    HOMOSAPIENS_TCR = json.load(s)
 with resource_stream(
     __name__,
-    'resources/tcr_d_designations_homosapiens.json') as s:
-    _TCR_D_DESIGNATIONS_HOMOSAPIENS = json.load(s)
-with resource_stream(
-    __name__,
-    'resources/tcr_synonyms_homosapiens.json') as s:
-    _TCR_SYNONYMS_HOMOSAPIENS = json.load(s)
+    'resources/homosapiens_tcr_synonyms.json') as s:
+    HOMOSAPIENS_TCR_SYNONYMS = json.load(s)
 
-PARSE_RE_HOMOSAPIENS_1 = re.compile(
-    r'^(TC?R)?([AB][VJ])(\d+)([S-](\d+))?((/|DV|/DV)(\d+))?(/?OR9-2)?([\*\.](.+))?$'
-)
-PARSE_RE_HOMOSAPIENS_2 = re.compile(
-    r'^(\d+)([S-](\d+))?(/?DV(\d+))([\*\.](\d+))?$'
-)
-
-
-with resource_stream(
-    __name__,
-    'resources/tcr_alleles_musmusculus.json') as s:
-    _TCR_ALLELES_MUSMUSCULUS = json.load(s)
-with resource_stream(
-    __name__,
-    'resources/tcr_d_designations_musmusculus.json') as s:
-    _TCR_D_DESIGNATIONS_MUSMUSCULUS = json.load(s)
-
-PARSE_RE_MUSMUSCULUS_1 = re.compile(
-    r'^TR([AB][VJ])(\d+)(D)?([S-](\d+))?((/|DV|/DV)(\d+)(D)?(-(\d))?)?([\*\.](.+))?$'
-)
-PARSE_RE_MUSMUSCULUS_2 = re.compile(
-    r'^(\d+)(D)?([S-](\d+))?(/?DV(\d+)(D)?(-(\d))?)([\*\.](\d+))?$'
-)
+PARSE_RE_HOMOSAPIENS_1 = re.compile(r'^([A-Z0-9\-\.\(\)\/]+)(\*([0-9]+))?$')
 
 
 # --- HELPER CLASSES ---
 
 
-class _DecomposedTCR(_DecomposedGene):
+class DecomposedTCR(_DecomposedGene):
+    def __init__(
+        self,
+        gene: str,
+        allele_designation: Optional[int],
+        ref_dict: dict,
+        syn_dict: dict
+    ) -> None:
+        self.gene = gene
+        self.allele_designation =\
+            f'{allele_designation:02}' if type(allele_designation) == int\
+            else None
+        self.ref_dict = ref_dict
+        self.syn_dict = syn_dict
+
+
     @property
     def valid(self) -> bool:
-        gene_str = self.compile(allele=False)
-
         # Is the gene valid?
-        if not gene_str in self.allele_dict:
+        if not self.gene in self.ref_dict:
             return False
         
         # If the gene exists and an allele is specified, check if it exists
-        if self.allele_num:
-            return self.allele_num in self.allele_dict[gene_str]
+        if self.allele_designation:
+            return self.allele_designation in self.ref_dict[self.gene]
         
         # If no allele specified, return true as the gene exists
         return True
-
-
-    def _resolve_allele_num(self) -> None:
-        '''
-        Either clean up allele_num if it contains something that looks like a
-        real number, otherwise set it to None.
-        '''
-        if self.allele_num is None:
-            return
-
-        if not self.allele_num.isdigit():
-            self.allele_num = None
-            return
-
-        if len(self.allele_num) == 1:
-            self.allele_num = '0' + self.allele_num
-
-
-class _DecomposedHomoSapiensTCR(_DecomposedTCR):
-    allele_dict = _TCR_ALLELES_HOMOSAPIENS
-
-    def __init__(
-        self,
-        base: Optional[str],
-        num1: Optional[str],
-        num2: Optional[str],
-        p: bool,
-        or92: bool,
-        d_designation: Optional[str],
-        allele_num: Optional[str]
-    ) -> None:
-        self.base = base
-        self.num1 = num1
-        self.num2 = num2
-        self.p = p
-        self.or92 = or92
-        self.d_designation = d_designation
-        self.allele_num = allele_num
     
 
     def compile(self, allele: bool = True) -> str:
-        compiled = 'TR' + self.base + self.num1
+        if allele and self.allele_designation:
+            return f'{self.gene}*{self.allele_designation}'
 
-        if self.num2:
-            compiled = compiled + '-' + self.num2
-
-        if self.p:
-            compiled = compiled + 'P'
-
-        if self.d_designation:
-            compiled = compiled + '/DV' + self.d_designation
-        
-        if self.or92:
-            compiled = compiled + '/OR9-2'
-        
-        if allele and self.allele_num:
-            compiled = compiled + '*' + self.allele_num
-
-        return compiled
-
+        return self.gene
+    
 
     def resolve(self) -> bool:
-        # Remove any leading zeros in gene numbers
-        self.num1 = None if self.num1 is None else self.num1.lstrip('0')
-        self.num2 = None if self.num2 is None else self.num2.lstrip('0')
-
-        # Resolve TRD designation if appropriate
-        if self.d_designation is None:
-            try:
-                self.d_designation = _TCR_D_DESIGNATIONS_HOMOSAPIENS[
-                    self.compile(allele=False)
-                ]
-
-            except(KeyError):
-                pass
-
-        # Add leading zero to allele number if appropriate
-        self._resolve_allele_num()
-
-        # If now a valid TCR, return True
-        if self.valid:
-            return True
-
-        # If still invalid, try nullifying num2 if appropriate
-        if self.num2 == '1':
-            self.num2 = None
-
-            if self.valid:
-                return True
-            
-            self.num2 = '1'
+        # If a synonym, correct to currently approved name
+        if self.syn_dict and self.gene in self.syn_dict:
+            self.gene = self.syn_dict[self.gene]
         
-        return False
-
-
-class _DecomposedMusMusculusTCR(_DecomposedTCR):
-    allele_dict = _TCR_ALLELES_MUSMUSCULUS
-
-    def __init__(
-        self,
-        base: Optional[str],
-        num1: Optional[str],
-        num1_d: bool,
-        num2: Optional[str],
-        dv_1: Optional[str],
-        dv_1_d: bool,
-        dv_2: Optional[str],
-        allele_num: Optional[str]
-    ) -> None:
-        self.base = base
-        self.num1 = num1
-        self.num1_d = num1_d
-        self.num2 = num2
-        self.dv_1 = dv_1
-        self.dv_1_d = dv_1_d
-        self.dv_2 = dv_2
-        self.allele_num = allele_num
-    
-
-    def compile(self, allele: bool = True) -> str:
-        compiled = 'TR' + self.base + self.num1
-
-        if self.num1_d:
-            compiled = compiled + 'D'
-
-        if self.num2:
-            compiled = compiled + '-' + self.num2
-
-        if self.dv_1:
-            compiled = compiled + '/DV' + self.dv_1
-
-        if self.dv_1_d:
-            compiled = compiled + 'D'
-
-        if self.dv_2:
-            compiled = compiled + '-' + self.dv_2
-        
-        if allele and self.allele_num:
-            compiled = compiled + '*' + self.allele_num
-
-        return compiled
-
-
-    def resolve(self) -> None:
-        # Remove any leading zeros in gene numbers
-        self.num1 = None if self.num1 is None else self.num1.lstrip('0')
-        self.num2 = None if self.num2 is None else self.num2.lstrip('0')
-
-        # Resolve TRD designation if appropriate
-        if self.dv_1 is None:
-            try:
-                d_designation = _TCR_D_DESIGNATIONS_MUSMUSCULUS[
-                    self.compile(allele=False)
-                ]
-                self.dv_1 = d_designation['dv_1']
-                self.dv_1_d = d_designation['dv_1_d']
-                self.dv_2 = d_designation['dv_2']
-
-            except(KeyError):
-                pass
-
-        # Add leading zero to allele number if appropriate
-        self._resolve_allele_num()
-
-        # If now a valid TCR, return True
-        if self.valid:
-            return True
-
-        # If still invalid, try nullifying num2 if appropriate
-        if self.num2 == '1':
-            self.num2 = None
-
-            if self.valid:
-                return True
-            
-            self.num2 = '1'
-        
-        return False
+        return self.valid
 
 
 # --- HELPER FUNCTIONS ---
 
 
-def _standardise_homosapiens(gene_name: str) -> str:
+def standardise_homosapiens(gene_name: str) -> str:
     # Take note of initial input for reference
     original_input = gene_name
 
@@ -261,123 +85,36 @@ def _standardise_homosapiens(gene_name: str) -> str:
     gene_name = ''.join(gene_name.split())
     gene_name = gene_name.replace('&nbsp;','')
 
-    # Parse attempt 1
-    if m := PARSE_RE_HOMOSAPIENS_1.match(gene_name):
-        gene_prefix = m.group(1)
-        base = m.group(2)
-        num1 = m.group(3)
-        num2 = m.group(5)
-        d_designation = m.group(8)
-        or92 = True if m.group(9) else False
-        allele_num = m.group(11)
+    # Capitalise
+    gene_name = gene_name.upper()
 
-        # If input looks like a deprecated name, see if it can be resolved to
-        # its modern couterpart via our synonym lookup table
-        if gene_prefix == 'TCR':
-            reconstructed = 'TCR' + base + num1
-            if num2:
-                reconstructed = reconstructed + 'S' + num2
-
-            try:
-                translation = _TCR_SYNONYMS_HOMOSAPIENS[reconstructed]
-                base = translation['base']
-                num1 = translation['num1']
-                num2 = translation['num2']
-                or92 = translation['OR9-2']
-
-            except(KeyError):
-                pass
-
-    # Parse attempt 2
-    elif m := PARSE_RE_HOMOSAPIENS_2.match(gene_name):
-        gene_prefix = None
-        base = 'AV'
-        num1 = m.group(1)
-        num2 = m.group(3)
-        d_designation = m.group(5)
-        or92 = False
-        allele_num = m.group(7)
+    # Parse attempt
+    if m := PARSE_RE_HOMOSAPIENS_1.match(gene_name): # ^([A-Z0-9\-\.\(\)\/]+)(\*([0-9]+))?$
+        gene = m.group(1)
+        allele_designation = None if m.group(3) is None else int(m.group(3))
 
     # Could not parse
     else:
-        _warn_failure(original_input, gene_name, 'Homo sapiens')
+        warn_failure(original_input, gene_name, 'Homo sapiens')
         return None
 
     # Build DecomposedTcr object
-    decomp_tcr = _DecomposedHomoSapiensTCR(
-        base=base,
-        num1=num1,
-        num2=num2,
-        p=False,
-        or92=or92,
-        d_designation=d_designation,
-        allele_num=allele_num
+    decomp_tcr = DecomposedTCR(
+        gene=gene,
+        allele_designation=allele_designation,
+        ref_dict=HOMOSAPIENS_TCR,
+        syn_dict=HOMOSAPIENS_TCR_SYNONYMS
     )
 
     # Try resolving, and return None on failure
     if not decomp_tcr.resolve():
-        _warn_failure(original_input, decomp_tcr.compile(), 'Homo sapiens')
+        warn_failure(original_input, decomp_tcr.compile(), 'Homo sapiens')
         return None
     
     return decomp_tcr.compile()
 
 
-def _standardise_musmusculus(gene_name: str) -> str:
-    # Take note of initial input for reference
-    original_input = gene_name
-
-    # Clean whitespace, remove known pollutors
-    gene_name = ''.join(gene_name.split())
-    gene_name = gene_name.replace('&nbsp;','')
-
-    # Parse attempt 1
-    if m := PARSE_RE_MUSMUSCULUS_1.match(gene_name): # ^TR([AB][VJ])(\d+)(D)?(-(\d+))?(/DV(\d+)(D)?(-(\d))?)?(\*(.+))?$
-        base = m.group(1)
-        num1 = m.group(2)
-        num1_d = True if m.group(3) else False
-        num2 = m.group(5)
-        dv_1 = m.group(8)
-        dv_1_d = True if m.group(9) else False
-        dv_2 = m.group(11)
-        allele_num = m.group(13)
-
-    # Parse attempt 2
-    elif m := PARSE_RE_MUSMUSCULUS_2.match(gene_name): # ^(\d+)(D)?([S-](\d+))?(/?DV(\d+)(D)?(-(\d))?)([\*\.](\d+))?$
-        base = 'AV'
-        num1 = m.group(1)
-        num1_d = True if m.group(2) else False
-        num2 = m.group(4)
-        dv_1 = m.group(6)
-        dv_1_d = True if m.group(7) else False
-        dv_2 = m.group(9)
-        allele_num = m.group(11)
-
-    # Could not parse
-    else:
-        _warn_failure(original_input, gene_name, 'Mus musculus')
-        return None
-
-    # Build DecomposedTcr object
-    decomp_tcr = _DecomposedMusMusculusTCR(
-        base=base,
-        num1=num1,
-        num1_d=num1_d,
-        num2=num2,
-        dv_1=dv_1,
-        dv_1_d=dv_1_d,
-        dv_2=dv_2,
-        allele_num=allele_num
-    )
-
-    # Try resolving, and return None on failure
-    if not decomp_tcr.resolve():
-        _warn_failure(original_input, decomp_tcr.compile(), 'Mus musculus')
-        return None
-    
-    return decomp_tcr.compile()
-
-
-def _warn_failure(
+def warn_failure(
     original_input: str,
     attempted_fix: str,
     species: str
@@ -393,8 +130,7 @@ def _warn_failure(
 
 
 SUPPORTED_SPECIES = {
-    'HomoSapiens': _standardise_homosapiens,
-    'MusMusculus': _standardise_musmusculus
+    'HomoSapiens': standardise_homosapiens
 }
 
 
@@ -417,8 +153,7 @@ def standardise(gene_name: str, species: str = 'HomoSapiens') -> str:
 
     # If gene_str is not a string, skip and return None
     if type(gene_name) != str:
-        _warn_failure(gene_name, gene_name, species)
-        return None
+        raise TypeError('gene_name must be a str.')
 
     # If the specified species is supported, attempt standardisation
     if species in SUPPORTED_SPECIES:
--- tests/test_tcr.py
+++ tests/test_tcr.py
@@ -2,178 +2,6 @@ import pytest
 from tidytcells import tcr
 
 
-class TestDecomposedHomoSapiensTcr:
-    def test_compile(self):
-        decomp_tcr = tcr._DecomposedHomoSapiensTCR(
-            base = 'AV',
-            num1 = '1',
-            num2 = '1',
-            p = False,
-            or92 = False,
-            d_designation = None,
-            allele_num = '01'
-        )
-
-        assert decomp_tcr.compile() == 'TRAV1-1*01'
-
-
-    def test_compile_with_dv(self):
-        decomp_tcr = tcr._DecomposedHomoSapiensTCR(
-            base='BV',
-            num1='20',
-            num2=None,
-            p=False,
-            or92=True,
-            d_designation=None,
-            allele_num='01'
-        )
-
-        assert decomp_tcr.compile() == 'TRBV20/OR9-2*01'
-    
-
-    def test_compile_with_or(self):
-        decomp_tcr = tcr._DecomposedHomoSapiensTCR(
-            base='AV',
-            num1='14',
-            num2=None,
-            p=False,
-            or92=False,
-            d_designation='4',
-            allele_num='01'
-        )
-
-        assert decomp_tcr.compile() == 'TRAV14/DV4*01'
-
-
-    def test_compile_without_allele(self):
-        decomp_tcr = tcr._DecomposedHomoSapiensTCR(
-            base = 'AV',
-            num1 = '1',
-            num2 = '1',
-            p = False,
-            or92 = False,
-            d_designation = None,
-            allele_num = '01'
-        )
-
-        assert decomp_tcr.compile(allele=False) == 'TRAV1-1'
-
-
-    def test_resolves_leading_zeros(self):
-        decomp_tcr = tcr._DecomposedHomoSapiensTCR(
-            base = 'AV',
-            num1 = '000002',
-            num2 = None,
-            p = False,
-            or92 = False,
-            d_designation = None,
-            allele_num = '01'
-        )
-
-        decomp_tcr.resolve()
-
-        assert decomp_tcr.num1 == '2'
-        assert decomp_tcr.num2 == None
-
-
-    def test_resolves_d_designation(self):
-        decomp_tcr = tcr._DecomposedHomoSapiensTCR(
-            base = 'AV',
-            num1 = '38',
-            num2 = '2',
-            p = False,
-            or92 = False,
-            d_designation = None,
-            allele_num = '01'
-        )
-
-        decomp_tcr.resolve()
-
-        assert decomp_tcr.d_designation == '8'
-
-
-    @pytest.mark.parametrize(
-        ('initial', 'expected'),
-        (
-            (None, None),
-            ('1', '01'),
-            ('01', '01'),
-            ('10', '10'),
-            ('foobar', None)
-        )
-    )
-    def test_resolves_allele_number(self, initial, expected):
-        decomp_tcr = tcr._DecomposedHomoSapiensTCR(
-            base = 'AV',
-            num1 = '1',
-            num2 = '1',
-            p = False,
-            or92 = False,
-            d_designation = None,
-            allele_num = initial
-        )
-
-        decomp_tcr.resolve()
-
-        assert decomp_tcr.allele_num == expected
-
-
-    @pytest.mark.parametrize(
-        ('decomp', 'expected'),
-        (
-            (('AV', '1', '1', False, False, None, '01'), '1'),
-            (('AV', '2', '1', False, False, None, '01'), None)
-        )
-    )
-    def test_resolves_num2_if_appropriate(self, decomp, expected):
-        decomp_tcr = tcr._DecomposedHomoSapiensTCR(*decomp)
-
-        decomp_tcr.resolve()
-
-        assert decomp_tcr.num2 == expected
-
-
-    @pytest.mark.parametrize(
-        'decomp',
-        (
-            ('AV', '1', '1', False, False, None, '01'),
-            ('BV', '20', None, False, True, None, '01'),
-            ('AJ', '58', None, False, False, None, '01')
-        )
-    )
-    def test_valid(self, decomp):
-        decomp_tcr = tcr._DecomposedHomoSapiensTCR(*decomp)
-
-        assert decomp_tcr.valid
-
-
-    @pytest.mark.parametrize(
-        'decomp',
-        (
-            ('AV', '99', None, False, False, None, None),
-            ('BV', '20', '2', False, False, None, None),
-            ('AJ', '58', None, False, False, None, '02')
-        )
-    )
-    def test_invalid(self, decomp):
-        decomp_tcr = tcr._DecomposedHomoSapiensTCR(*decomp)
-
-        assert not decomp_tcr.valid
-    
-
-    @pytest.mark.parametrize(
-        ('decomp', 'expected'),
-        (
-            (('AV', '1', '1', False, False, None, '01'), True),
-            (('AV', '01', '00001', False, False, None, '1'), True)
-        )
-    )
-    def test_returns_resolve_success(self, decomp, expected):
-        decomp_tcr = tcr._DecomposedHomoSapiensTCR(*decomp)
-
-        assert decomp_tcr.resolve() == expected
-
-
 class TestStandardise:
     @pytest.mark.parametrize(
         'species',
@@ -226,9 +54,7 @@ class TestStandardiseHomoSapiens:
         (
             'foobar', 
             'TRAV3D-3*01', 
-            '', 
-            1234, 
-            None
+            ''
         )
     )
     def test_bad_tcr(self, gene):
@@ -242,31 +68,28 @@ class TestStandardiseHomoSapiens:
 
 
     @pytest.mark.parametrize(
-        ('gene', 'expected'),
+        'gene',
         (
-            ('TRAV3*01&nbsp;', 'TRAV3*01'),
-            ('TRBV07-01*01', 'TRBV7-1*01')
+            1234, 
+            None
         )
     )
-    def test_remove_pollutants(self, gene, expected):
-        result = tcr.standardise(
-            gene_name=gene,
-            species='HomoSapiens'
-        )
-
-        assert result == expected
+    def test_bad_type(self, gene):
+        with pytest.raises(TypeError):
+            result = tcr.standardise(
+                gene_name=gene,
+                species='HomoSapiens'
+            )
 
 
     @pytest.mark.parametrize(
         ('gene', 'expected'),
-        (  
-            ('TRAV38-2DV8*01', 'TRAV38-2/DV8*01'),
-            ('TRAV14DV4', 'TRAV14/DV4'),
-            ('TRAV29DV5', 'TRAV29/DV5'),
-            ('TRBV20OR9-2', 'TRBV20/OR9-2')
+        (
+            ('TRAV3*01&nbsp;', 'TRAV3*01'),
+            (' TRAV3 * 01 ', 'TRAV3*01')
         )
     )
-    def test_adds_slashes(self, gene, expected):
+    def test_remove_pollutants(self, gene, expected):
         result = tcr.standardise(
             gene_name=gene,
             species='HomoSapiens'
@@ -279,9 +102,8 @@ class TestStandardiseHomoSapiens:
         ('gene', 'expected'),
         (
             ('TCRAV32S1', 'TRAV25'),
-            ('TCRAV14-2', 'TRAV38-1'),
-            ('TCRBV21S1*01', 'TRBV11-1*01'),
-            ('TCRBV12S3', 'TRBV12-3')
+            ('TCRAV14S2', 'TRAV38-1'),
+            ('TCRBV21S1*01', 'TRBV11-1*01')
         )
     )
     def test_resolve_alternate_tcr_names(self, gene, expected):
@@ -293,189 +115,133 @@ class TestStandardiseHomoSapiens:
         assert result == expected
 
 
-    @pytest.mark.parametrize(
-        ('gene', 'expected'),
-        (
-            ('TCRAV14/4', 'TRAV14/DV4'),
-            ('36DV7*02', 'TRAV36/DV7*02'),
-            ('TCRAV9-2*01/02/03/04', 'TRAV9-2'),
-            ('TRAJ42.1', 'TRAJ42*01')
-        )
-    )
-    def test_various_typos(self, gene, expected):
-        result = tcr.standardise(
-            gene_name=gene,
-            species='HomoSapiens'
-        )
+# class TestStandardiseMusMusculus:
+#     @pytest.mark.parametrize(
+#         'gene',
+#         (
+#             'TRAV3-1*01',
+#             'TRAJ15*01',
+#             'TRBV5*02',
+#             'TRBJ2-1*01',
+#             'TRAJ15',
+#             'TRAV21/DV12',
+#             'TRAV3D-3',
+#             'TRAV15D-1/DV6D-1'
+#         )
+#     )
+#     def test_already_correctly_formatted(self, gene):
+#         result = tcr.standardise(
+#             gene_name=gene,
+#             species='MusMusculus'
+#         )
 
-        assert result == expected
-
-
-    def test_resolves_d_designation(self):
-        result = tcr.standardise(
-            gene_name='TRAV23',
-            species='HomoSapiens'
-        )
-
-        assert result == 'TRAV23/DV6'
+#         assert result == gene
 
 
-    @pytest.mark.parametrize(
-        'gene', ('TRAV36/DV1', 'TRAV1-1/DV1')
-    )
-    def test_fails_if_detected_av_dv_designation_mismatch(self, gene):
-        with pytest.warns(UserWarning, match='Unrecognised'):
-            result = tcr.standardise(
-                gene_name=gene,
-                species='HomoSapiens'
-            )
+#     @pytest.mark.parametrize(
+#         'gene',
+#         (
+#             'foobar', 
+#             'TRAV3*01', 
+#             '', 
+#             1234, 
+#             None
+#         )
+#     )
+#     def test_bad_tcr(self, gene):
+#         with pytest.warns(UserWarning, match='Unrecognised'):
+#             result = tcr.standardise(
+#                 gene_name=gene,
+#                 species='MusMusculus'
+#             )
 
-        assert result == None
-
-
-    @pytest.mark.parametrize(
-        ('gene', 'expected'),
-        (
-            ('TRAV2-1', 'TRAV2'),
-            ('TRAV14-1/DV4', 'TRAV14/DV4')
-        )
-    )
-    def test_removes_unnecessary_specifiers(self, gene, expected):
-        result = tcr.standardise(
-            gene_name=gene,
-            species='HomoSapiens'
-        )
-
-        assert result == expected
+#         assert result == None
 
 
-class TestStandardiseMusMusculus:
-    @pytest.mark.parametrize(
-        'gene',
-        (
-            'TRAV3-1*01',
-            'TRAJ15*01',
-            'TRBV5*02',
-            'TRBJ2-1*01',
-            'TRAJ15',
-            'TRAV21/DV12',
-            'TRAV3D-3',
-            'TRAV15D-1/DV6D-1'
-        )
-    )
-    def test_already_correctly_formatted(self, gene):
-        result = tcr.standardise(
-            gene_name=gene,
-            species='MusMusculus'
-        )
+#     @pytest.mark.parametrize(
+#         ('gene', 'expected'),
+#         (
+#             ('TRAV3D-3*01&nbsp;', 'TRAV3D-3*01'),
+#             ('TRAV09D-01*01', 'TRAV9D-1*01')
+#         )
+#     )
+#     def test_remove_pollutants(self, gene, expected):
+#         result = tcr.standardise(
+#             gene_name=gene,
+#             species='MusMusculus'
+#         )
 
-        assert result == gene
+#         assert result == expected
 
 
-    @pytest.mark.parametrize(
-        'gene',
-        (
-            'foobar', 
-            'TRAV3*01', 
-            '', 
-            1234, 
-            None
-        )
-    )
-    def test_bad_tcr(self, gene):
-        with pytest.warns(UserWarning, match='Unrecognised'):
-            result = tcr.standardise(
-                gene_name=gene,
-                species='MusMusculus'
-            )
+#     @pytest.mark.parametrize(
+#         ('gene', 'expected'),
+#         (  
+#             ('TRAV4-4DV10*01', 'TRAV4-4/DV10*01'),
+#             ('TRAV16DDV11', 'TRAV16D/DV11'),
+#             ('TRAV15D-2DV6D-2', 'TRAV15D-2/DV6D-2')
+#         )
+#     )
+#     def test_adds_slashes(self, gene, expected):
+#         result = tcr.standardise(
+#             gene_name=gene,
+#             species='MusMusculus'
+#         )
 
-        assert result == None
+#         assert result == expected
 
 
-    @pytest.mark.parametrize(
-        ('gene', 'expected'),
-        (
-            ('TRAV3D-3*01&nbsp;', 'TRAV3D-3*01'),
-            ('TRAV09D-01*01', 'TRAV9D-1*01')
-        )
-    )
-    def test_remove_pollutants(self, gene, expected):
-        result = tcr.standardise(
-            gene_name=gene,
-            species='MusMusculus'
-        )
+#     @pytest.mark.parametrize(
+#         ('gene', 'expected'),
+#         (
+#             ('TRAV14D-3/8', 'TRAV14D-3/DV8'),
+#             ('21DV12*02', 'TRAV21/DV12*02'),
+#             ('TRAV9D-2*01/02/03/04', 'TRAV9D-2'),
+#             ('TRAJ42.1', 'TRAJ42*01')
+#         )
+#     )
+#     def test_various_typos(self, gene, expected):
+#         result = tcr.standardise(
+#             gene_name=gene,
+#             species='MusMusculus'
+#         )
 
-        assert result == expected
-
-
-    @pytest.mark.parametrize(
-        ('gene', 'expected'),
-        (  
-            ('TRAV4-4DV10*01', 'TRAV4-4/DV10*01'),
-            ('TRAV16DDV11', 'TRAV16D/DV11'),
-            ('TRAV15D-2DV6D-2', 'TRAV15D-2/DV6D-2')
-        )
-    )
-    def test_adds_slashes(self, gene, expected):
-        result = tcr.standardise(
-            gene_name=gene,
-            species='MusMusculus'
-        )
-
-        assert result == expected
+#         assert result == expected
 
 
-    @pytest.mark.parametrize(
-        ('gene', 'expected'),
-        (
-            ('TRAV14D-3/8', 'TRAV14D-3/DV8'),
-            ('21DV12*02', 'TRAV21/DV12*02'),
-            ('TRAV9D-2*01/02/03/04', 'TRAV9D-2'),
-            ('TRAJ42.1', 'TRAJ42*01')
-        )
-    )
-    def test_various_typos(self, gene, expected):
-        result = tcr.standardise(
-            gene_name=gene,
-            species='MusMusculus'
-        )
+#     def test_resolves_d_designation(self):
+#         result = tcr.standardise(
+#             gene_name='TRAV21',
+#             species='MusMusculus'
+#         )
 
-        assert result == expected
+#         assert result == 'TRAV21/DV12'
 
 
-    def test_resolves_d_designation(self):
-        result = tcr.standardise(
-            gene_name='TRAV21',
-            species='MusMusculus'
-        )
+#     @pytest.mark.parametrize(
+#         'gene', ('TRAV13-4/DV1', 'TRAV4-4/DV1')
+#     )
+#     def test_fails_if_detected_av_dv_designation_mismatch(self, gene):
+#         with pytest.warns(UserWarning, match='Unrecognised'):
+#             result = tcr.standardise(
+#                 gene_name=gene,
+#                 species='MusMusculus'
+#             )
 
-        assert result == 'TRAV21/DV12'
-
-
-    @pytest.mark.parametrize(
-        'gene', ('TRAV13-4/DV1', 'TRAV4-4/DV1')
-    )
-    def test_fails_if_detected_av_dv_designation_mismatch(self, gene):
-        with pytest.warns(UserWarning, match='Unrecognised'):
-            result = tcr.standardise(
-                gene_name=gene,
-                species='MusMusculus'
-            )
-
-        assert result == None
+#         assert result == None
 
 
-    @pytest.mark.parametrize(
-        ('gene', 'expected'),
-        (
-            ('TRAV19-1', 'TRAV19'),
-            ('TRAV21-1/DV12', 'TRAV21/DV12')
-        )
-    )
-    def test_removes_unnecessary_specifiers(self, gene, expected):
-        result = tcr.standardise(
-            gene_name=gene,
-            species='MusMusculus'
-        )
+#     @pytest.mark.parametrize(
+#         ('gene', 'expected'),
+#         (
+#             ('TRAV19-1', 'TRAV19'),
+#             ('TRAV21-1/DV12', 'TRAV21/DV12')
+#         )
+#     )
+#     def test_removes_unnecessary_specifiers(self, gene, expected):
+#         result = tcr.standardise(
+#             gene_name=gene,
+#             species='MusMusculus'
+#         )
 
-        assert result == expected
+#         assert result == expected
