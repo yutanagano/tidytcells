137755e571d7b09f10cc6745a72298adf5cc7b1d
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2023-06-02 23:25:02 +0100

    Make code backwards compatible with py >=37

--- src/tidytcells/_resources/__init__.py
+++ src/tidytcells/_resources/__init__.py
@@ -1,25 +1,30 @@
+from importlib import resources
 import json
-from importlib.resources import files
+import sys
+
+if sys.version_info >= (3, 9):
+
+    def get_json_resource(filename: str) -> dict:
+        with resources.files(__name__).joinpath(filename).open("r") as f:
+            return json.load(f)
+
+else:
+
+    def get_json_resource(filename: str) -> dict:
+        with resources.open_text(__name__, filename) as f:
+            return json.load(f)
 
 
-with files(__name__).joinpath("homosapiens_tcr.json").open("rb") as f:
-    HOMOSAPIENS_TCR = json.load(f)
-with files(__name__).joinpath("homosapiens_tcr_synonyms.json").open("rb") as f:
-    HOMOSAPIENS_TCR_SYNONYMS = json.load(f)
-with files(__name__).joinpath("homosapiens_tcr_aa_sequences.json").open("rb") as f:
-    HOMOSAPIENS_TCR_AA_SEQUENCES = json.load(f)
-with files(__name__).joinpath("homosapiens_mhc.json").open("rb") as f:
-    HOMOSAPIENS_MHC = json.load(f)
-with files(__name__).joinpath("homosapiens_mhc_synonyms.json").open("rb") as f:
-    HOMOSAPIENS_MHC_SYNONYMS = json.load(f)
+HOMOSAPIENS_TCR = get_json_resource("homosapiens_tcr.json")
+HOMOSAPIENS_TCR_SYNONYMS = get_json_resource("homosapiens_tcr_synonyms.json")
+HOMOSAPIENS_TCR_AA_SEQUENCES = get_json_resource("homosapiens_tcr_aa_sequences.json")
+HOMOSAPIENS_MHC = get_json_resource("homosapiens_mhc.json")
+HOMOSAPIENS_MHC_SYNONYMS = get_json_resource("homosapiens_mhc_synonyms.json")
 
 
-with files(__name__).joinpath("musmusculus_tcr.json").open("rb") as f:
-    MUSMUSCULUS_TCR = json.load(f)
-with files(__name__).joinpath("musmusculus_mhc.json").open("rb") as f:
-    MUSMUSCULUS_MHC = json.load(f)
-with files(__name__).joinpath("musmusculus_mhc_synonyms.json").open("rb") as f:
-    MUSMUSCULUS_MHC_SYNONYMS = json.load(f)
+MUSMUSCULUS_TCR = get_json_resource("musmusculus_tcr.json")
+MUSMUSCULUS_MHC = get_json_resource("musmusculus_mhc.json")
+MUSMUSCULUS_MHC_SYNONYMS = get_json_resource("musmusculus_mhc_synonyms.json")
 
 
 AMINO_ACIDS = frozenset(
