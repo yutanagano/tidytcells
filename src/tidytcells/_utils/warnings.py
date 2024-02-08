c3680b2d790ea341a789357e284f2941d3ea159b
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-02-08 22:15:59 +0000

    Lint with black

--- src/tidytcells/_utils/warnings.py
+++ src/tidytcells/_utils/warnings.py
@@ -4,10 +4,10 @@ from warnings import warn
 def warn_failure(
     reason_for_failure: str, original_input: str, attempted_fix: str, species: str
 ):
-    warning_message = f"Failed to standardize \"{original_input}\" for species {species}: {reason_for_failure}."
+    warning_message = f'Failed to standardize "{original_input}" for species {species}: {reason_for_failure}.'
 
     if original_input != attempted_fix:
-        warning_message += f" (best attempted fix: \"{attempted_fix}\")."
+        warning_message += f' (best attempted fix: "{attempted_fix}").'
 
     warn(warning_message)
 
