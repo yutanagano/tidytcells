eab126dda8481049093f96408215e1d1121caf3c
Lonneke Scheffer <lonsch96@live.nl>
2025-11-17 16:23:08 -0800

    removed obsolete comment

--- src/tidytcells/_standardized_gene_symbol/standardized_gene_symbol.py
+++ src/tidytcells/_standardized_gene_symbol/standardized_gene_symbol.py
@@ -16,14 +16,6 @@ class StandardizedSymbol(ABC):
     def __init__(self, symbol: str) -> None:
         pass
 
-    # @abstractmethod
-    # def get_reason_why_invalid(self, enforce_functional: bool = False) -> Optional[str]:
-    #     """
-    #     If the gene cannot be standardized (it is invalid), this method returns a string outlining the reason why (nonexistent gene, not functional, etc.).
-    #     Returns None if standardisation was successful.
-    #     If enforce_functional is set to True, then symbols of valid but non-functional genes will be rejected.
-    #     """
-
 
 class StandardizedReceptorGeneSymbol(StandardizedSymbol):
 
