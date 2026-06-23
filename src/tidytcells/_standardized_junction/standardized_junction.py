b811d32e3c4ad0a768c71a7e717f4488f4f839a6
Lonneke Scheffer <lonsch96@live.nl>
2026-06-23 15:32:50 -0700

    improved error messages for junction standardization

--- src/tidytcells/_standardized_junction/standardized_junction.py
+++ src/tidytcells/_standardized_junction/standardized_junction.py
@@ -63,11 +63,14 @@ class JunctionStandardizer(ABC):
         self.j_alignments = self.align_j()
         self.v_alignments = self.align_v()
 
-        self.corrected_seq = self.correct_seq_j_side(self.corrected_seq)
-        self.corrected_seq = self.correct_seq_v_side(self.corrected_seq)
+        if len(self.j_alignments) > 0:
+            self.corrected_seq = self.correct_seq_j_side(self.corrected_seq)
+
+        if len(self.v_alignments) > 0:
+            self.corrected_seq = self.correct_seq_v_side(self.corrected_seq)
 
         if len(self.corrected_seq) < 6:
-            self.reasons_invalid.append("junction too short")
+            self.reasons_invalid.append("Junction too short")
 
     def get_aa_dict_from_symbol(self, gene) -> dict:
         '''
@@ -86,13 +89,13 @@ class JunctionStandardizer(ABC):
         # if symbol is an allele, return only info for the given allele
         if "*" in symbol:
             if gene not in symbol:
-                self.reasons_invalid.append(f"not a {gene} gene: {symbol}")
+                self.reasons_invalid.append(f"not a {gene} gene: {symbol} ({self._species})")
                 return dict()
 
             if symbol in  self._sequence_dictionary:
                 return {symbol: self._sequence_dictionary[symbol]}
             else:
-                self.reasons_invalid.append("no sequence information known for " + symbol)
+                self.reasons_invalid.append(f"no sequence information known for {symbol} ({self._species})")
 
         # if symbol is less specific than allele, retrieve any alleles that are valid extensions of the given symbol
         enforce_functional = self.enforce_functional_v if gene == "V" else self.enforce_functional_j
@@ -175,8 +178,11 @@ class JunctionStandardizer(ABC):
 
         if len(best_alignments) == 0:
             err = "J alignment unsuccessful"
+
             if self.j_symbol is not None:
-                err += " for J symbol " + self.j_symbol
+                err += f" for J symbol {self.j_symbol} ({self._species})"
+            else:
+                err += f" for any J symbol ({self._species})"
 
             if len(self.j_aa_dict) == 0:
                 err += ": no known sequence information"
@@ -197,8 +203,11 @@ class JunctionStandardizer(ABC):
 
         if len(best_alignments) == 0:
             err = "V alignment unsuccessful"
+
             if self.v_symbol is not None:
-                err += " for V symbol " + self.v_symbol
+                err += f" for V symbol {self.v_symbol} ({self._species})"
+            else:
+                err += " for any V symbol"
 
             if len(self.v_aa_dict) == 0:
                 err += ": no known sequence information"
@@ -218,6 +227,8 @@ class JunctionStandardizer(ABC):
         '''
 
         corrected_seqs = set()
+
+        alignment_too_long = []
 
         for alignment_details in self.j_alignments:
             j_conserved_idx = alignment_details["j_conserved_idx"]
@@ -241,6 +252,8 @@ class JunctionStandardizer(ABC):
 
                     reconstructed_aas = alignment_details["j_region"][start_j_idx:end_j_idx]
                     corrected_seqs.add(seq + reconstructed_aas)
+                else:
+                    alignment_too_long.append(f"{alignment_details['gene']} ({self._species}): Alignment successful but reconstruction too long ({reconstruction_length})")
 
         if len(corrected_seqs) > 1:
             # When Junction-reconstruction results are ambiguous (different J's)
@@ -259,7 +272,8 @@ class JunctionStandardizer(ABC):
                 return seq
 
         if len(corrected_seqs) == 0:
-            self.reasons_invalid.append(f"J side reconstruction unsuccessful.")
+            self.reasons_invalid.append(f"J side reconstruction unsuccessful")
+            self.reasons_invalid.extend(alignment_too_long)
             return seq
 
         return corrected_seqs.pop()
@@ -274,6 +288,7 @@ class JunctionStandardizer(ABC):
           2. An unambiguous correction can be determined (no disagreement between alignments)
         '''
         corrected_seqs = set()
+        alignment_too_long = []
 
         for alignment_details in self.v_alignments:
             v_conserved_idx = alignment_details["v_conserved_idx"]
@@ -289,10 +304,13 @@ class JunctionStandardizer(ABC):
 
             if v_conserved_idx < v_offset:
                 reconstructed_aas = alignment_details["v_region"][v_conserved_idx:][:v_offset]
+                reconstruction_length = len(reconstructed_aas)
 
-                if len(reconstructed_aas) <= self.max_v_reconstruction:
+                if reconstruction_length <= self.max_v_reconstruction:
                     new_seq = reconstructed_aas + seq
                     corrected_seqs.add(new_seq)
+                else:
+                    alignment_too_long.append(f"{alignment_details['gene']} ({self._species}): Alignment successful but reconstruction too long ({reconstruction_length})")
 
         corrected_seqs = {seq for seq in corrected_seqs if seq.startswith("C")}
 
@@ -300,7 +318,8 @@ class JunctionStandardizer(ABC):
             return "C" + seq
 
         if len(corrected_seqs) == 0:
-            self.reasons_invalid.append(f"V side reconstruction unsuccessful.")
+            self.reasons_invalid.append(f"V side reconstruction unsuccessful")
+            self.reasons_invalid.extend(alignment_too_long)
             return seq
 
         if len(corrected_seqs) > 1:
